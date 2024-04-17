aaef80f631565710d835a3c483c2325e9379cc71
levitimothy <levitimothy@mail.weber.edu>
2024-04-16 19:04:35 -0600

    finished assignment

--- mysite/touchgrass/views.py
+++ mysite/touchgrass/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import Paginator
 
 
 def home(request):
     post = Post.objects.all()
+    username = request.GET.get('username')
+    if username != '' and username is not None:
+        post = post.filter(username__icontains=username)
     newpost = post[::-1]
+    paginator = Paginator(newpost, 5)
+    page = request.GET.get('page')
+    newpost = paginator.get_page(page)
     return render(request, 'touchgrass/home.html', {'post': newpost})
 
 
